064ff0dcef246246cba731d245aa035bf28faaa8
BIT Student <classroom@bgit.rs>
2019-06-18 14:40:07 +0200

    about page

---
+++ src/view/About/AboutPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div className="about-content">
+            <article>
+                <h1>ABOUT</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure perspiciatis at eveniet facilis animi dolore aliquid voluptatibus. Magni, eveniet velit quia facilis tempora libero enim adipisci? Aliquid, consectetur soluta? Illo, sunt eligendi? Labore unde reprehenderit quasi, tempora architecto consequuntur veritatis? Voluptatem possimus deserunt iure, corporis doloremque quibusdam earum nostrum facere quidem esse odit quasi ea perferendis minima assumenda provident voluptas atque aliquid. Ea id totam eaque odio ipsum quos, maiores maxime aspernatur perspiciatis sapiente consectetur distinctio deleniti asperiores fugit veritatis omnis enim vel obcaecati earum nemo quia! Pariatur velit rerum architecto ullam natus ipsa sint sunt, voluptate minima animi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, nemo omnis atque cumque facilis asperiores voluptate officia, culpa, harum illum vitae veritatis dolore quaerat in consequatur itaque odit dignissimos. Recusandae at facilis ducimus magnam doloribus aliquid voluptatum iure maxime, itaque tempore placeat fuga sunt. At, quasi. Sint excepturi, earum eveniet modi qui sequi corporis odio, doloremque distinctio maiores, esse voluptates molestiae perferendis magni minus. Pariatur repellendus asperiores, facilis delectus cupiditate, nobis fugit modi aspernatur ex, nisi aut officiis eum rem quis et soluta autem quisquam iste voluptas voluptate numquam architecto. Corrupti voluptatibus id vitae necessitatibus, magni nobis adipisci sequi laborum.</p>
+            </article>
+            <article>
+                <h1>OUR STORY</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, minima autem doloremque dignissimos nemo dicta, temporibus laudantium consequatur fugit ad neque explicabo velit illum reprehenderit amet. Voluptate, corporis sequi doloribus consectetur asperiores quam fuga cum libero excepturi cupiditate quos, ratione neque rerum aperiam maxime, obcaecati earum ullam nostrum et autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, nemo omnis atque cumque facilis asperiores voluptate officia, culpa, harum illum vitae veritatis dolore quaerat in consequatur itaque odit dignissimos. Recusandae at facilis ducimus magnam doloribus aliquid voluptatum iure maxime, itaque tempore placeat fuga sunt. At, quasi. Sint excepturi, earum eveniet modi qui sequi corporis odio, doloremque distinctio maiores, esse voluptates molestiae perferendis magni minus. Pariatur repellendus asperiores, facilis delectus cupiditate, nobis fugit modi aspernatur ex, nisi aut officiis eum rem quis et soluta autem quisquam iste voluptas voluptate numquam architecto. Corrupti voluptatibus id vitae necessitatibus, magni nobis adipisci sequi laborum.</p>
+            </article>
+
+        </div>
+    )
+}
+export {
+    AboutPage
+}
--- src/view/Main.js
+++ src/view/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { HomePage } from './Home/Homepage';
 import { Route, Switch } from "react-router-dom";
+import { AboutPage } from './About/AboutPage'
 
 
 const Main = () => {
     return (
         <main>
             <Switch>
+                <Route path="/about" component={AboutPage} />
                 <Route path="/" component={HomePage} />
             </Switch>
         </main>
