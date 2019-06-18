a42f704b94dbf3ddd80a64810e6a90f0f9987ea9
Milica <mici17>
2019-06-18 21:30:53 +0200

    single post css

--- src/view/SinglePost/SinglePost.js
+++ src/view/SinglePost/SinglePost.js
@@ -5,13 +5,14 @@ import { authors } from '../../shared/authors'
 export const SinglePost = (props) => {
 
     const post = posts.find(post => `${post.id}` === props.match.params.id);
+    // const author=authors.find(author)
 
     return (
-        <div>
+        <div className="single-post"> 
             <span>Back</span>
-            <h1>{post.title}</h1>
+            <h1 className="single-post-title">{post.title}</h1>
             <span>{authors.name}</span>
-            <article>
+            <article className="single-post-body">
                 {post.body}
             </article>
         </div>
